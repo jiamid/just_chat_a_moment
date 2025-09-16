e49ac139ebe8290c15ed1fc3255b7cc3323659f8
Jiamid <JiamidTan@poweronlabs.ai>
2025-09-16 19:49:30 +0800

    feat:local test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-
+from contextlib import asynccontextmanager
 from .db import Base, engine
 from .auth import router as auth_router, get_current_user
 from .rooms import router as ws_router
+from loguru import logger
 
-app = FastAPI(title="Just Chat A Moment API")
+
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    logger.info("🚀 Starting Application")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    logger.info(f"Docs http://127.0.0.1:8000/docs")
+    yield
+    logger.info("⛔ Stopping Application")
+
+
+app = FastAPI(title="Just Chat A Moment API", lifespan=lifespan)
 
 # CORS（前端开发友好）
 app.add_middleware(
@@ -15,13 +27,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.on_event("startup")
-async def on_startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
 
 app.include_router(auth_router, prefix="/api")
 app.include_router(ws_router)
--- backend/local_main.py
+++ backend/local_main.py
@@ -10,4 +10,4 @@ from app.main import app
 
 if __name__ == '__main__':
     from uvicorn import run
-    run(app, host='0.0.0.0', port=8000,log_config=None)
+    run(app, host='0.0.0.0', port=8000)
