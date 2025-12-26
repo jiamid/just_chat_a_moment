72ef73267600d1c602eef7325b6b14249cf840ae
jiamid <jiamid@qq.com>
2025-12-26 22:38:59 +0800

    fix:enter

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -796,6 +796,7 @@ export default {
     0 8px 16px rgba(0, 0, 0, 0.15),
     0 4px 8px rgba(0, 0, 0, 0.1),
     inset 0 1px 0 rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(10px);
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
